2c163324b9bd3992d0c6b087a89619089b4111b9
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-06-11 23:28:31 +0530

    adding state in CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>RS 899</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
-                    <div className="cart-item-actions">   </div>
+                    <div style={ {fontSize: 25} }>{title}</div>
+                    <div style={ {color: '#777'} }>Rs {price}</div>
+                    <div style={ {color: '#777'} }>Qty: {qty}</div>
+                    <div className="cart-item-actions"> 
+                       {/* {Buttons} */}
+                       <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png"/>
+                       <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"/>
+                       <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png"/>
+                    </div>
                 </div>
             </div>
         )
@@ -20,10 +35,10 @@ class CartItem extends React.Component {
 
 const styles = {
     image: {
-        height: 110,
-        width: 110,
+        height: 128,
+        width: 122,
         borderRadius: 5,
-        backgroundColor: 'blue',
+        backgroundColor: '#ccc',
     }
 }
 
