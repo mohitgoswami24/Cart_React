3a75a3d96123331f2effcecac7e6fd24ff896732
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-06-11 22:14:41 +0530

    setting up react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const name = 'Mohit';
+  const isLoggedIn = false;
+
+
+  return (
+    <div className="App">
+     <h1>Cart</h1>
+    </div>
+  );
+}
+
+export default App;
