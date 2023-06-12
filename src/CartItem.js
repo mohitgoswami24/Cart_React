bc98688ea2432ad0ce1e0d660c3563f12c89acf0
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-06-12 10:04:07 +0530

    add increase quantity fun

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 class CartItem extends React.Component {
+   
     constructor () {
         super();
         this.state = {
@@ -9,9 +10,14 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this)
+    }
+    increaseQuantity = () => {
+        console.log('this.state',this.state);
     }
     render() {
         const { price, title, qty } = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -23,9 +29,22 @@ class CartItem extends React.Component {
                     <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions"> 
                        {/* {Buttons} */}
-                       <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png"/>
-                       <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"/>
-                       <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png"/>
+                       <img 
+                          alt="increase" 
+                          className="action-icons" 
+                          src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+                          onClick={this.increaseQuantity}
+                        />
+                       <img 
+                          alt="decrease" 
+                          className="action-icons" 
+                          src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+                        />
+                       <img
+                          alt="delete" 
+                          className="action-icons" 
+                          src="https://cdn-icons-png.flaticon.com/128/484/484662.png" 
+                        />
                     </div>
                 </div>
             </div>
