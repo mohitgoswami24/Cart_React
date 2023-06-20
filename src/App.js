a705ba28b493062194cba12ff0a0b0a3a0e46077
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-06-20 20:08:12 +0530

    Reading data from firebase / keeping the firebase in sync with UI / Adding data to firebase

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./NavBar";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 class App extends React.Component {
   constructor () {
     super();
     this.state = {
-       products: [
-        {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: 'https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
-            id: 1
-        },
-        {
-            price: 99,
-            title: 'Watch',
-            qty: 1,
-            img: 'https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHdhdGNofGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
-            id: 2
-        },
-        {
-            price: 999,
-            title: 'Laptop',
-            qty: 1,
-            img: 'https://plus.unsplash.com/premium_photo-1681566925246-cc584a44d7fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-            id: 3
-        },
-       ]
+       products: [],
+       loading: true
     }   
+  this.db = firebase.firestore();
 }
+
+componentDidMount () {
+  this.db
+    .collection('products')
+    .onSnapshot((snapshot) => {
+      console.log(snapshot);
+
+      snapshot.docs.map((doc)=>{
+        console.log(doc.data())
+      });
+
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data();
+
+        data['id'] = doc.id;
+        return data;
+      })
+
+      this.setState({
+        products,
+        loading: false
+      })
+    })
+}
+
 handleIncreaseQuantity = (product) => {
   console.log('Hey please inc the qty of ', product);
   const { products } = this.state;
@@ -92,17 +99,36 @@ getCartTotal = () => {
   return cartTotal;
 }
 
+addProduct = () => {
+  this.db
+    .collection('products')
+    .add({
+      img: '',
+      price: 900,
+      qty: 3,
+      title: 'washing machine'
+    })
+    .then((docRef) => {
+      console.log('Product has been added',docRef)
+    })
+    .catch((error) => {
+      console.log('Erro : ', error);
+    })
+}
+
 render(){
-  const { products } = this.state;
+  const { products, loading } = this.state;
   return (
     <div className="App">
      <Navbar count={this.getCartCount()}/>
+     <button onClick={this.addProduct} style={{ padding: 20, fontSize: 20}}>Add a product</button>
      <Cart 
         products={products}
         onIncreaseQuantity={this.handleIncreaseQuantity}
         onDecreaseQuantity={this.handleDecreaseQuantity}
         onDeleteProduct={this.handleDeleteProduct}
      />
+     {loading && <h1>Loading Products...</h1>}
      <div style={ {fontSize:20, padding:10} }>TOTAL: {this.getCartTotal()}</div>
     </div>
   );
